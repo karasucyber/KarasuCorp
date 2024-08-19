5a00e7a9ffa4b1226bc80ef0ba72b85fbc3c85d0
karasucyber <karasucyber@gmail.com>
2024-08-19 19:19:26 -0300

    up

--- WebSite/app/page.tsx
+++ WebSite/app/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { GiHamburgerMenu } from "react-icons/gi";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
@@ -9,36 +9,36 @@ import Link from 'next/link';
 import { FaWhatsapp } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 
-
-
-
-
 export default function Home() {
   useEffect(() => {
     AOS.init();
   }, []);
 
-   addEventListener('load', function() {
-    const loader = document.getElementById('loader');
-    if (loader) {
-      loader.style.display = 'none';
-    }
-  });
-  
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000); // Tempo em milissegundos
+
+    return () => clearTimeout(timer); // Limpeza do timer
+  }, []);
 
   return (
-    
-    <main className="">
-      <div id="loader">
-  <div className="spinner"></div>
-</div>
-      <div >
-        <div className="primary-section" data-aos="fade-up" >
+    <main>
+      {loading && (
+        <div id="loader">
+          <div className="spinner"></div>
+        </div>
+      )}
+      <div className={loading ? 'hidden' : ''}>
+        <div className="primary-section" data-aos="fade-up">
           <div className="hamburger" data-aos="fade-left">
             <GiHamburgerMenu />
           </div>
           <div className="logo-section">
-            <a className="Logo1" data-aos="fade-left">KARASU</a><p className="a-Corp" data-aos="fade-left">CORP</p>
+            <a className="Logo1" data-aos="fade-left">KARASU</a>
+            <p className="a-Corp" data-aos="fade-left">CORP</p>
             <div className="Section-in-primary">
               <img src="karasu.png" data-aos="fade-up" alt="Karasu Logo" />
             </div>
@@ -52,100 +52,87 @@ export default function Home() {
           <div className="sectiom-in-sencundary">
             <h1 data-aos="fade-right" data-aos-delay="0">Construindo Legados Digitais</h1>
             <div className="sencudary-h6">
-              <h6 data-aos="fade-right" data-aos-delay="0"> Construindo pontes entre passado e</h6>
+              <h6 data-aos="fade-right" data-aos-delay="0">Construindo pontes entre passado e</h6>
               <h6 data-aos="fade-right" data-aos-delay="0">futuro digital</h6>
             </div>
-            <button className="button-secundary" data-aos="fade-right" > Construa</button>
+            <button className="button-secundary" data-aos="fade-right">Construa</button>
           </div>
         </div>
-      </div>
-      <div className="three-section">
-  <div className="three-section-right">
-    <h1>Quem somos</h1>
-    <div className="Container-Mac">
-      <Mac />
-    </div>
-  </div>
-
-  <div className="three-section-left">
-  <div className="typing-text">
-    Somos uma <a className="Palavras_Chaves">startup de tecnologia e inovação</a> que acredita na harmonia entre o clássico e o moderno. Nossa essência é trazer soluções inovadoras sem perder o bom e velho toque old school que tanto valorizamos. Com uma cultura que honra o passado e abraça o futuro, integramos <a className="Palavras_Chaves">inteligência artificial</a> em seus sistemas <a className="Palavras_Chaves">ERP</a> para otimizar processos e melhorar a tomada de decisões.
-  </div>
-  <br />
-  <div className="typing-text">
-    Além disso, ajudamos você a vender mais, criando <a className="Palavras_Chaves">sites robustos e eficientes</a>, projetados para atender às demandas do mercado atual. Nosso objetivo é ser um <a className="Palavras_Chaves">parceiro estratégico</a>, fornecendo as ferramentas que você precisa para alcançar o sucesso em um mundo em constante mudança.
-  </div>
-</div>
-</div>
-
-      <div className="four-section">
-        <h1>Nossos Serviços</h1><ol>
-          <li>
-            <div className="icon"><i className="fa-solid fa-bicycle"></i></div>
-            <a>-</a>
-            <div className="title">Websites</div>
-          </li>
-          <li>
-            <div className="icon"><i className="fa-solid fa-car"></i></div>
-            <a>-</a>
-            <div className="title">E-commerce</div>
-          </li>
-          <li>
-            <div className="icon"><i className="fa-solid fa-helicopter"></i></div>
-            <a>-</a>
-            <div className="title">Marketing Digital</div>
-          </li>
-          <li>
-            <div className="icon"><i className="fa-solid fa-plane"></i></div>
-            <a>-</a>
-            <div className="title">Gerenciamneto de Tráfego</div>
-          </li>
-          <li>
-            <div className="icon"><i className="fa-solid fa-rocket"></i></div>
-            <a>-</a>
-            <div className="title">Dashboards Interativos</div>
-          </li>
-          <li>
-            <div className="icon"><i className="fa-solid fa-bus"></i></div>
-            <a>-</a>
-            <div className="title">
-              Softwares
-              Personalizado
+        <div className="three-section">
+          <div className="three-section-right">
+            <h1>Quem somos</h1>
+            <div className="Container-Mac">
+              <Mac />
             </div>
-          </li>
-          <li>
-            <div className="icon"><i className="fa-solid fa-bus"></i></div>
-            <a>-</a>
-            <div className="title">Designer</div>
-          </li>
-        </ol>
-
-        <div className='six-section'>
-          <h1>Gostaria de ver nossos projetos?</h1>
-          <Link href="/Projetos" passHref>
-            <button className="button-secundary">Projetos</button>
-          </Link>
+          </div>
+          <div className="three-section-left">
+            <div className="typing-text">
+              Somos uma <a className="Palavras_Chaves">startup de tecnologia e inovação</a> que acredita na harmonia entre o clássico e o moderno. Nossa essência é trazer soluções inovadoras sem perder o bom e velho toque old school que tanto valorizamos. Com uma cultura que honra o passado e abraça o futuro, integramos <a className="Palavras_Chaves">inteligência artificial</a> em seus sistemas <a className="Palavras_Chaves">ERP</a> para otimizar processos e melhorar a tomada de decisões.
+            </div>
+            <br />
+            <div className="typing-text">
+              Além disso, ajudamos você a vender mais, criando <a className="Palavras_Chaves">sites robustos e eficientes</a>, projetados para atender às demandas do mercado atual. Nosso objetivo é ser um <a className="Palavras_Chaves">parceiro estratégico</a>, fornecendo as ferramentas que você precisa para alcançar o sucesso em um mundo em constante mudança.
+            </div>
+          </div>
         </div>
+
+        <div className="four-section">
+          <h1>Nossos Serviços</h1>
+          <ol>
+            <li>
+              <div className="icon"><i className="fa-solid fa-bicycle"></i></div>
+              <a>-</a>
+              <div className="title">Websites</div>
+            </li>
+            <li>
+              <div className="icon"><i className="fa-solid fa-car"></i></div>
+              <a>-</a>
+              <div className="title">E-commerce</div>
+            </li>
+            <li>
+              <div className="icon"><i className="fa-solid fa-helicopter"></i></div>
+              <a>-</a>
+              <div className="title">Marketing Digital</div>
+            </li>
+            <li>
+              <div className="icon"><i className="fa-solid fa-plane"></i></div>
+              <a>-</a>
+              <div className="title">Gerenciamento de Tráfego</div>
+            </li>
+            <li>
+              <div className="icon"><i className="fa-solid fa-rocket"></i></div>
+              <a>-</a>
+              <div className="title">Dashboards Interativos</div>
+            </li>
+            <li>
+              <div className="icon"><i className="fa-solid fa-bus"></i></div>
+              <a>-</a>
+              <div className="title">Softwares Personalizados</div>
+            </li>
+            <li>
+              <div className="icon"><i className="fa-solid fa-bus"></i></div>
+              <a>-</a>
+              <div className="title">Designer</div>
+            </li>
+          </ol>
+          <div className='six-section'>
+            <h1>Gostaria de ver nossos projetos?</h1>
+            <Link href="/Projetos" passHref>
+              <button className="button-secundary">Projetos</button>
+            </Link>
+          </div>
+        </div>
+        <a href="https://www.instagram.com/karasu_corp" target="_blank" rel="noopener noreferrer">
+          <div className={`Insta-Box ${loading ? 'hidden' : ''}`}>
+            <FaInstagram style={{ width: "80%", height: "100%" }} />
+          </div>
+        </a>
+        <a href="https://wa.me/011919216813" target="_blank" rel="noopener noreferrer">
+          <div className={`Whats-Box ${loading ? 'hidden' : ''}`}>
+            <FaWhatsapp style={{ width: "100%", height: "100%" }} />
+          </div>
+        </a>
       </div>
-      <a href="https://www.instagram.com/karasu_corp" target="_blank" rel="noopener noreferrer">
-  <div className="Insta-Box">
-    <FaInstagram style={{ width: "80%", height: "100%" }} />
-  </div>
-</a>
-
-<a href="https://wa.me/011919216813" target="_blank" rel="noopener noreferrer">
-  <div className="Whats-Box">
-    <FaWhatsapp style={{ width: "100%", height: "100%" }} />
-  </div>
-</a>
-
     </main>
-    
   );
 }
-
-
-
-/*  <div className="five-section">
-        <Scroll2></Scroll2>
-      </div> */
