23ef78f7e0118e982d5d8f1fa40db030d4b506f3
karasucyber <karasucyber@gmail.com>
2024-08-30 17:15:13 -0300

    up

--- WebSite/app/Componentes/forms/forms.tsx
+++ WebSite/app/Componentes/forms/forms.tsx
@@ -1,32 +1,72 @@
-import React from "react";
+import React, { useRef, useState, FormEvent } from "react";
 import "./styles.css";
 import { AiOutlineArrowUp } from "react-icons/ai";
+import emailjs from "@emailjs/browser";
 
-const Forms = () => {
+const Forms: React.FC = () => {
+  const form = useRef<HTMLFormElement | null>(null);
+  const [popupMessage, setPopupMessage] = useState<string | null>(null);
+
+  const sendEmail = (e: FormEvent) => {
+    e.preventDefault();
+
+    if (form.current) {
+      emailjs
+        .sendForm("service_o6gpste", "template_nilrfzk", form.current, "lLbscr2I_Ijwmz7nW")
+        .then(
+          () => {
+            setPopupMessage("Mensagem enviada com sucesso !");
+          },
+          (error: { text: string }) => {
+            setPopupMessage(`Falha ao enviar mensagem: ${error.text}`);
+          }
+        );
+    }
+  };
+
+  const closePopup = () => {
+    setPopupMessage(null);
+  };
+
   return (
     <div className="Container-Forms">
       <h1 className="text">Vamos elevar seu patamar?</h1>
-      <h3 className="text">Uma pequena mensagem pode mudar seu negócio</h3> 
+      <h3 className="text">Uma pequena mensagem pode mudar seu negócio</h3>
       <div className="container3">
-        <img
-          src="Karasu .png"
-          alt="Karasu Logo"
-          className="container3-img"  // Adicione uma classe para aplicar estilos específicos se necessário
-        />
-        <form className="container3-form" aria-labelledby="form-header">
+        <img src="Karasu .png" alt="Karasu Logo" className="container3-img" />
+        <form
+          ref={form}
+          onSubmit={sendEmail}
+          className="container3-form"
+          aria-labelledby="form-header"
+        >
           <div className="box-forms">
             <label htmlFor="email">Email</label>
             <textarea
               id="email"
+              name="email"
               className="textarea-Forms"
               placeholder="Digite seu e-mail"
               aria-required="true"
             />
           </div>
+
+          <div className="box-forms">
+            <label htmlFor="empresa">Sua empresa</label>
+            <textarea
+              id="empresa"
+              name="empresa"
+              className="textarea-Forms"
+              placeholder="Digite o nome ou CNPJ"
+              aria-required="true"
+            />
+          </div>
+
           <div className="box-forms">
             <label htmlFor="whatsapp">WhatsApp</label>
             <textarea
               id="whatsapp"
+              name="whatsapp"
               className="textarea-Forms"
               placeholder="Digite seu número de WhatsApp"
               aria-required="true"
@@ -36,6 +76,7 @@ const Forms = () => {
             <label htmlFor="services">Serviços requeridos</label>
             <textarea
               id="services"
+              name="services"
               className="textarea-Forms"
               placeholder="Descreva os serviços que você precisa"
               aria-required="true"
@@ -45,13 +86,14 @@ const Forms = () => {
             <label htmlFor="investment">Investimento</label>
             <select
               id="investment"
+              name="investment"
               className="Select-Forms"
               aria-required="true"
             >
-              <option value="baixo">R$ 500 Até R$ 1.500</option>
-              <option value="médio">Até R$ 5.000</option>
-              <option value="alto">R$ 5.000 - R$ 20.000</option>
-              <option value="muito-alto">Acima de R$ 20.000</option>
+              <option value="R$ 500 Até R$ 1.500">R$ 500 Até R$ 1.500</option>
+              <option value="Até R$ 5.000">Até R$ 5.000</option>
+              <option value="R$ 5.000 - R$ 20.000">R$ 5.000 - R$ 20.000</option>
+              <option value="Acima de R$ 20.000">Acima de R$ 20.000</option>
             </select>
           </div>
           <div className="box-forms">
@@ -61,6 +103,17 @@ const Forms = () => {
           </div>
         </form>
       </div>
+
+      {popupMessage && (
+        <div className="popup">
+          <div className="popup-content">
+            <p>{popupMessage}</p>
+            <button onClick={closePopup} aria-label="Fechar Popup">
+              Fechar
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
